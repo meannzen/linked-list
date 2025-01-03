7faeb7921e9fe810f7adf3b76f4323661e2cda1d
meannzen <senmeann@gmail.com>
2025-01-03 15:25:08 +0700

    fmt

--- src/second.rs
+++ src/second.rs
@@ -9,13 +9,12 @@ struct Node<T> {
     next: Link<T>,
 }
 
-
 pub struct IntoIter<T>(List<T>);
-pub struct Iter<'a ,T> {
-    next: Option<&'a Node<T>>
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
 }
 
-impl <'a, T> Iterator for Iter<'a, T> {
+impl<'a, T> Iterator for Iter<'a, T> {
     type Item = &'a T;
     fn next(&mut self) -> Option<Self::Item> {
         self.next.map(|node| {
@@ -41,8 +40,10 @@ impl<T> List<T> {
         IntoIter(self)
     }
 
-    pub fn iter<'a>(&'a self)->Iter<'a, T> {
-        Iter { next: self.head.as_deref() }
+    pub fn iter<'a>(&'a self) -> Iter<'a, T> {
+        Iter {
+            next: self.head.as_deref(),
+        }
     }
 
     pub fn push(&mut self, elem: T) {
@@ -128,8 +129,10 @@ mod test {
     #[test]
     fn iter() {
         let mut list = crate::second::List::new();
-        list.push(1); list.push(2); list.push(3);
-    
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
         let mut iter = list.iter();
         assert_eq!(iter.next(), Some(&3));
         assert_eq!(iter.next(), Some(&2));
